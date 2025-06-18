1a1146d30fab6bcf127b09b5b4164eb8b052c352
seolgi <jsg5383@gmail.com>
2025-06-19 00:15:20 +0900

    CLI認証に変更

--- src/main/java/com/fl/restapi/config/AwsConfig.java
+++ src/main/java/com/fl/restapi/config/AwsConfig.java
@@ -3,8 +3,6 @@ package com.fl.restapi.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.lambda.LambdaClient;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -15,18 +13,10 @@ public class AwsConfig {
     @Value("${aws.region}")
     private String region;
 
-    @Value("${aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${aws.credentials.secret-key}")
-    private String secretKey;
-
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
     }
 
@@ -34,8 +24,6 @@ public class AwsConfig {
     public LambdaClient lambdaClient() {
         return LambdaClient.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
     }
 }
