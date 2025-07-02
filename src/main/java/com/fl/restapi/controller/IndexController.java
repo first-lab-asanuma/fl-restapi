4045fda9ecd69b620a04176e61498ce5a943e7a9
seolgi <jsg5383@gmail.com>
2025-07-03 01:20:53 +0900

    API GateWay方法追加

--- src/main/java/com/fl/restapi/controller/IndexController.java
+++ src/main/java/com/fl/restapi/controller/IndexController.java
@@ -2,14 +2,20 @@ package com.fl.restapi.controller;
 
 import com.fl.restapi.reqeust.CallLambdaReq;
 import com.fl.restapi.service.S3Service;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,10 +26,13 @@ public class IndexController {
 
     private final S3Service s3Service;
 
+    @Value("${aws.gateway.endpoint-url}")
+    private String endpointUrl;
+
     @GetMapping("/")
     public String index() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HH:mm:ss_SSS");
-        return "Hello World now: " + LocalDateTime.now().format(formatter);
+        return "Hello World Now: " + LocalDateTime.now().format(formatter);
     }
 
     @SneakyThrows
@@ -40,6 +49,32 @@ public class IndexController {
         s3Service.invokeLambdaFunctionAsync(payload);
 
         return "callLambda req: + " + req;
+    }
+
+    @SneakyThrows
+    @PostMapping("/call-gateway-lambda")
+    public String callGatewayLambda(
+            @RequestBody CallLambdaReq req
+    ) {
+
+        try {
+            Map<String, Object> requestBody = new HashMap<>();
+            requestBody.put("orderId", req.orderId());
+            requestBody.put("amount", req.amount());
+            requestBody.put("item", req.item());
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
+
+            RestTemplate restTemplate = new RestTemplate();
+
+            restTemplate.postForEntity(endpointUrl, requestEntity, String.class);
+        } catch (Exception e) {
+            return "error";
+        }
+        return "success";
     }
 
     @PostMapping("/put-s3")
